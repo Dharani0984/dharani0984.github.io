70c273201b4fd872010903e17e24015c5aab84f1
dharani0984 <nellore.dharani0984@gmail.com>
2023-07-05 11:03:07 +0530

    update Login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\User;
 
-
-
 class LoginController extends Controller
 {
     /**
@@ -21,7 +19,6 @@ class LoginController extends Controller
      */
     public function login(Request $request)
     {
-        // Validate the request data
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
             'password' => 'required|string',
@@ -31,20 +28,13 @@ class LoginController extends Controller
             return response()->json(['error' => $validator->errors()], 400);
         }
 
-        // Attempt to authenticate the user
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['error' => 'Invalid credentials'], 401);
         }
 
-        // Generate JWT token for the authenticated user
-       // $token = auth()->attempt($request->only('email', 'password'));
-
-
        $user = User::find(Auth::id()); // Assuming you have a user with ID 1
        $token = JWTAuth::fromUser($user);
 
-
-        // Return the JWT token in the response
         return response()->json(['status'=>'true','user' => Auth::id(),'token' => $token]);
     }
 }
